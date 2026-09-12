2e25c8508a7300a0a1c3fe7750edc3407d4116df
MathisBruel <mathis.bruel17700@gmail.com>
2026-09-12 22:44:09 +0200

    feat: enable dynamic rendering for ContratPage component

--- app/(public)/stands/contrat/page.tsx
+++ app/(public)/stands/contrat/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 
+export const dynamic = 'force-dynamic'
 export const metadata: Metadata = { title: 'Contrat de location de stand' }
 
 export default function ContratPage() {
